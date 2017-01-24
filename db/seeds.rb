c531a74a79bb5646653b3a2ace9dfc152cef35b0
Andre <andreggv@gmail.com>
2017-01-24 21:46:20 -0200

    Add models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+movies = [ 
+
+
+	{title: "As Good As It Gets", release_date: "25-Nov-1992"},
+	{title: "When Harry Met Sally", release_date: "20-Dez-1995"},
+	{title: "The help", release_date: "25-Nov-1997"},
+	{title: "Titanic", release_date: "06-Ago-1997"}
+
+
+]
+
+movies.each do |movie|
+	Movie.create!(movie)
+
+end
